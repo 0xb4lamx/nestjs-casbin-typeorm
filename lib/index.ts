87fa7a3122aa1a7dbf0dc2458088562509c7028b
0xb4lamx <medghaith.sellami@gmail.com>
2020-02-17 16:14:41 +0100

    fix: export interfaces

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
+export * from "./interfaces";
 export * from "./casbin.constants";
 export * from "./casbin.module";
 export * from "./casbin.service";
---
+++ lib/interfaces/index.ts
@@ -0,0 +1,1 @@
+export * from "./casbin-options.interface"
